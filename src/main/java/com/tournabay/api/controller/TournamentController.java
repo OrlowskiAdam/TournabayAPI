7f1edd35a64fb39f3667d35c228426f167e8935a
Adam Orłowski <orlowski.adam.99@gmail.com>
2021-09-14 17:37:59 +0200

    Created endpoint for tournament data searched by id

--- src/main/java/com/tournabay/api/controller/TournamentController.java
+++ src/main/java/com/tournabay/api/controller/TournamentController.java
@@ -3,7 +3,6 @@ package com.tournabay.api.controller;
 import com.tournabay.api.model.Tournament;
 import com.tournabay.api.model.User;
 import com.tournabay.api.payload.CreateTournamentRequest;
-import com.tournabay.api.repository.UserRepository;
 import com.tournabay.api.security.CurrentUser;
 import com.tournabay.api.security.UserPrincipal;
 import com.tournabay.api.service.TournamentService;
@@ -11,10 +10,7 @@ import com.tournabay.api.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,11 +19,17 @@ public class TournamentController {
     private final UserService userService;
     private final TournamentService tournamentService;
 
+    @GetMapping("/tournament/{id}")
+    public ResponseEntity<Tournament> getTournamentById(@PathVariable Long id) {
+        Tournament tournament = tournamentService.getTournamentById(id);
+        return ResponseEntity.ok(tournament);
+    }
+
     @PostMapping("/tournament/create")
     @Secured("ROLE_USER")
     public ResponseEntity<Tournament> createTournament(@CurrentUser UserPrincipal userPrincipal, @RequestBody CreateTournamentRequest body) {
         User user = userService.getUserFromPrincipal(userPrincipal);
-        Tournament tournament = tournamentService.createTournament(body.getName(), body.getScoreType(), body.getStartDate(), body.getEndDate(), user);
+        Tournament tournament = tournamentService.createTournament(body, user);
         return ResponseEntity.ok(tournament);
     }
 }
