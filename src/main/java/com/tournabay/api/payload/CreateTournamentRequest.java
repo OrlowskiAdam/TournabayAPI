e621f18d722f2cb75c1fe99cf1686f1b876445d7
Adam Orłowski <orlowski.adam.99@gmail.com>
2021-09-14 17:37:05 +0200

    Updated CreateTournamentRequest.java to handle Game mode and Team Format enums

--- src/main/java/com/tournabay/api/payload/CreateTournamentRequest.java
+++ src/main/java/com/tournabay/api/payload/CreateTournamentRequest.java
@@ -1,6 +1,8 @@
 package com.tournabay.api.payload;
 
+import com.tournabay.api.model.GameMode;
 import com.tournabay.api.model.ScoreType;
+import com.tournabay.api.model.TeamFormat;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -11,6 +13,8 @@ import java.time.LocalDateTime;
 public class CreateTournamentRequest {
     private String name;
     private ScoreType scoreType;
+    private GameMode gameMode;
+    private TeamFormat teamFormat;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
 }
