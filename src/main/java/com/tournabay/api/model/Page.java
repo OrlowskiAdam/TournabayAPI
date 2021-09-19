eb9822d968310d587cf9f419d6167c952c95bbfc
Adam Orłowski <orlowski.adam.99@gmail.com>
2021-09-19 23:28:10 +0200

    Fixed Page.java

--- src/main/java/com/tournabay/api/model/Page.java
+++ src/main/java/com/tournabay/api/model/Page.java
@@ -1,13 +1,20 @@
 package com.tournabay.api.model;
 
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor
 public class Page {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
     private String path;
 
     @OneToMany
