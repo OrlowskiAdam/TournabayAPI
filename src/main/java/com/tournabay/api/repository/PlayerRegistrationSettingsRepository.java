0eff5ee2c24ba91c746cb6e417388a506df41b1b
Adam Orłowski <orlowski.adam.99@gmail.com>
2022-05-09 20:17:24 +0200

    Removed unused entity, persistence and business logic

--- src/main/java/com/tournabay/api/repository/PlayerRegistrationSettingsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tournabay.api.repository;
-
-import com.tournabay.api.model.settings.PlayerRegistrationSettings;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlayerRegistrationSettingsRepository extends JpaRepository<PlayerRegistrationSettings, Long> {
-}
