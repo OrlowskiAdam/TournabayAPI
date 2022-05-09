8060dc7a735b495bf79c761f8eba05c40127ca3b
Adam Orłowski <orlowski.adam.99@gmail.com>
2022-05-09 20:16:44 +0200

    Removed unnecessary variable from PlayerBasedTournament.java

--- src/main/java/com/tournabay/api/model/PlayerBasedTournament.java
+++ src/main/java/com/tournabay/api/model/PlayerBasedTournament.java
@@ -1,13 +1,11 @@
 package com.tournabay.api.model;
 
-import com.tournabay.api.model.settings.PlayerBasedTournamentSettings;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
-import javax.persistence.OneToOne;
 
 @Entity
 @NoArgsConstructor
@@ -15,7 +13,5 @@ import javax.persistence.OneToOne;
 @Setter
 @SuperBuilder
 public class PlayerBasedTournament extends Tournament {
-    @OneToOne(mappedBy = "tournament")
-    private PlayerBasedTournamentSettings tournamentSettings;
 
 }
