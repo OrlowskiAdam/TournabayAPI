0cf4d78e8c36eb38d27c2c91c9ced9dda899a2f9
Adam Orłowski <orlowski.adam.99@gmail.com>
2022-05-09 20:18:01 +0200

    Added SettingsRepository.java

--- src/main/java/com/tournabay/api/repository/SettingsRepository.java
+++ src/main/java/com/tournabay/api/repository/SettingsRepository.java
@@ -1,9 +1,9 @@
 package com.tournabay.api.repository;
 
-import com.tournabay.api.model.settings.TournamentSettings;
+import com.tournabay.api.model.Settings;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TournamentSettingsRepository extends JpaRepository<TournamentSettings, Long> {
+public interface SettingsRepository extends JpaRepository<Settings, Long> {
 }
