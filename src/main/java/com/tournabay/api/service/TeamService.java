7fbb1ce2c57efe566df7aa665ed54e173321b830
Adam Orłowski <orlowski.adam.99@gmail.com>
2022-07-23 13:28:36 +0200

    Added methods for updating and deleting teams

--- src/main/java/com/tournabay/api/service/TeamService.java
+++ src/main/java/com/tournabay/api/service/TeamService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -36,18 +38,18 @@ public class TeamService {
         throw new BadRequestException("Invalid tournament type!");
     }
 
-
     /**
-     * Creates a team for a team based tournament
+     * Create a team for a team based tournament
      *
-     * @param name The name of the team
-     * @param participantIds A list of participant IDs that will be added to the team.
-     * @param seed The seed of the team.
-     * @param teamStatus The status of the team.
-     * @param tournament The tournament that the team is being created for.
-     * @return A Team object
+     * @param name           The name of the team
+     * @param captainId      The id of the captain of the team
+     * @param participantIds List of participant ids that will be in the team
+     * @param seed           The seed of the team.
+     * @param teamStatus     The status of the team.
+     * @param tournament     The tournament that the team is being created for.
+     * @return A new team is being returned.
      */
-    public Team createTeam(String name, List<Long> participantIds, Seed seed, TeamStatus teamStatus, Tournament tournament) {
+    public Team createTeam(String name, Long captainId, List<Long> participantIds, Seed seed, TeamStatus teamStatus, Tournament tournament) {
         if (tournament instanceof TeamBasedTournament) {
             TeamBasedTournament teamBasedTournament = (TeamBasedTournament) tournament;
             // Check if the team name is unique
@@ -59,11 +61,11 @@ public class TeamService {
             if (participantIds.size() > maxTeamSize)
                 throw new BadRequestException("Team size cannot be greater than " + maxTeamSize);
             // Check if the participants are not in other teams
-            List<Participant> participants = participantService.getAllByIds(participantIds, tournament);
+            List<Participant> participants = participantService.getAllByIds(new HashSet<>(participantIds), tournament);
             if (checkForDuplicatedParticipants(participants, teamBasedTournament))
                 throw new BadRequestException("One or more participant is already in other team!");
             // First participant in the list is the captain
-            Participant captain = participants.size() > 0 ? participants.get(0) : null;
+            Participant captain = participantService.getById(captainId, tournament);
             Team team = Team.builder()
                     .name(name)
                     .seed(seed)
@@ -72,10 +74,22 @@ public class TeamService {
                     .participants(new HashSet<>(participants))
                     .tournament(teamBasedTournament)
                     .build();
-            Team newTeam = teamRepository.save(team);
-            participants.forEach(participant -> participant.setTeam(newTeam));
-            participantService.saveAll(participants);
-            return newTeam;
+            return teamRepository.save(team);
+        }
+        throw new BadRequestException("Invalid tournament type!");
+    }
+
+    /**
+     * If the tournament is a team based tournament, then delete the team and set all the participants' team to null.
+     *
+     * @param tournament The tournament that the team is being removed from.
+     * @param team       The team to be removed.
+     * @return The team that was removed.
+     */
+    public Team removeTeam(Tournament tournament, Team team) {
+        if (tournament instanceof TeamBasedTournament) {
+            teamRepository.delete(team);
+            return team;
         }
         throw new BadRequestException("Invalid tournament type!");
     }
@@ -91,10 +105,55 @@ public class TeamService {
     }
 
     /**
+     * If the tournament is a team based tournament, return the team with the given id, otherwise throw an exception.
+     *
+     * @param teamId     The id of the team we want to get.
+     * @param tournament The tournament that the team belongs to.
+     * @return A team object
+     */
+    public Team getById(Long teamId, Tournament tournament) {
+        if (tournament instanceof TeamBasedTournament) {
+            return teamRepository.findById(teamId).orElseThrow(() -> new ResourceNotFoundException("Team not found!"));
+        }
+        throw new BadRequestException("Invalid tournament type!");
+    }
+
+    public Team updateTeam(Tournament tournament, Team team, String name, Long captainId, List<Long> participantIds, Seed seed, TeamStatus status) {
+        if (tournament instanceof TeamBasedTournament) {
+            TeamBasedTournament teamBasedTournament = (TeamBasedTournament) tournament;
+            if (!team.getName().equals(name) && checkForUniqueTeamName(name, teamBasedTournament))
+                throw new BadRequestException("Team name already exists!");
+            // Check if the team size is valid
+            Settings settings = teamBasedTournament.getSettings();
+            Integer maxTeamSize = settings.getMaxTeamSize();
+            if (participantIds.size() > maxTeamSize)
+                throw new BadRequestException("Team size cannot be greater than " + maxTeamSize);
+            // Check if the participants are not in other teams
+            List<Participant> participants = participantService.getAllByIds(new HashSet<>(participantIds), tournament);
+            Set<Participant> currentParticipants = team.getParticipants();
+            List<Participant> newParticipants = participants.stream()
+                    .filter(participant -> !currentParticipants.contains(participant))
+                    .collect(Collectors.toList());
+            if (checkForDuplicatedParticipants(newParticipants, teamBasedTournament))
+                throw new BadRequestException("One or more participant is already in other team!");
+            team.getParticipants().forEach(participant -> participant.setTeam(null));
+            team.getCaptain().setTeam(null);
+            Participant captain = participantService.getById(captainId, tournament);
+            team.setName(name);
+            team.setCaptain(captain);
+            team.setParticipants(new HashSet<>(participants));
+            team.setSeed(seed);
+            team.setStatus(status);
+            return teamRepository.save(team);
+        }
+        throw new BadRequestException("Invalid tournament type!");
+    }
+
+    /**
      * Check if any of the participants in the list of participants are already in the tournament's team
      *
      * @param participants The list of participants that are being added to the team.
-     * @param tournament The tournament to check for duplicate participants in.
+     * @param tournament   The tournament to check for duplicate participants in.
      * @return A boolean value.
      */
     private boolean checkForDuplicatedParticipants(List<Participant> participants, TeamBasedTournament tournament) {
@@ -111,7 +170,7 @@ public class TeamService {
     /**
      * Check if any of the teams in the tournament have the same name as the one passed in.
      *
-     * @param name The name of the team to be created
+     * @param name       The name of the team to be created
      * @param tournament The tournament that the team is being added to.
      * @return A boolean value.
      */
