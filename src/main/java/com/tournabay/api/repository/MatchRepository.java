5f8eb367cfe0d91029ddee45971b1f36ad5ba641
Adam Orłowski <orlowski.adam.99@gmail.com>
2022-07-24 11:47:39 +0200

    TB-1 Created match repository

---
+++ src/main/java/com/tournabay/api/repository/MatchRepository.java
@@ -0,0 +1,9 @@
+package com.tournabay.api.repository;
+
+import com.tournabay.api.model.Match;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MatchRepository extends JpaRepository<Match, Long> {
+}
