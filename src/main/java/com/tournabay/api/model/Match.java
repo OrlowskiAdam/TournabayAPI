ac48659ae9f51e985c9ddaa21af77b6c6ece14bb
Adam Orłowski <orlowski.adam.99@gmail.com>
2022-07-30 11:18:43 +0200

    TB-1 Updated Match.java to handle staff limit per match, added match result, tournament relationship and postload LocalDateTime

--- src/main/java/com/tournabay/api/model/Match.java
+++ src/main/java/com/tournabay/api/model/Match.java
@@ -1,12 +1,17 @@
 package com.tournabay.api.model;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Set;
 
@@ -16,25 +21,60 @@ import java.util.Set;
 @NoArgsConstructor
 @DynamicUpdate
 @Table(name = "\"match\"")
-public class Match {
+public abstract class Match {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     private LocalDate startDate;
 
+    @NotNull
     private LocalTime startTime;
 
+    @Transient
+    private LocalDateTime ldt;
+
+    @NotNull
     private Boolean isCompleted;
 
+    @NotNull
     private Boolean isLive;
 
-    @OneToMany
+    @NotNull
+    @Min(value = 1, message = "Referees limit must be greater than 0")
+    private Integer refereesLimit;
+
+    @NotNull
+    @Min(value = 1, message = "Commentators limit must be greater than 0")
+    private Integer commentatorsLimit;
+
+    @NotNull
+    @Min(value = 1, message = "Streamers limit must be greater than 0")
+    private Integer streamersLimit;
+
+    @ManyToMany
     private Set<StaffMember> referees;
 
-    @OneToMany
+    @ManyToMany
     private Set<StaffMember> commentators;
 
-    @OneToMany
+    @ManyToMany
     private Set<StaffMember> streamers;
+
+    @OneToOne
+    private MatchResult matchResult;
+
+    @JsonIdentityInfo(
+            generator = ObjectIdGenerators.PropertyGenerator.class,
+            property = "id"
+    )
+    @ManyToOne
+    private Tournament tournament;
+
+    @PostLoad
+    private void postLoad() {
+        this.ldt = LocalDateTime.of(this.startDate, this.startTime);
+    }
 }
