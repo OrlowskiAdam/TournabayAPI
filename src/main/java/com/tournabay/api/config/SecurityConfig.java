a644bad1c773262315f2399006badd4450a61eed
Adam Orłowski <orlowski.adam.99@gmail.com>
2022-07-31 10:49:29 +0200

    TB-17 Removed @EnableGlobalMethodSecurity annotation causing bean problems

--- src/main/java/com/tournabay/api/config/SecurityConfig.java
+++ src/main/java/com/tournabay/api/config/SecurityConfig.java
@@ -26,11 +26,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-@EnableGlobalMethodSecurity(
-        securedEnabled = true,
-        jsr250Enabled = true,
-        prePostEnabled = true
-)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomUserDetailsService customUserDetailsService;
     private final CustomOAuth2UserService customOAuth2UserService;
