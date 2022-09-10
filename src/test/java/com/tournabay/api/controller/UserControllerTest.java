7004148683a1c66326cc0775040034273141a653
Adam Orłowski <orlowski.adam.99@gmail.com>
2022-09-11 00:10:59 +0200

    Added simple test

---
+++ src/test/java/com/tournabay/api/controller/UserControllerTest.java
@@ -0,0 +1,36 @@
+package com.tournabay.api.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("dev")
+class UserControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void asd() throws Exception {
+//        mockMvc.perform(get("/api/hello"))
+//                .andExpect(status().isOk())
+//                .andExpect(content().string("Hello"));
+    }
+}
