33d85036393149d49a52470bf3db6d61ed35cee8
Adam Orłowski <orlowski.adam.99@gmail.com>
2022-11-06 23:06:29 +0100

    Created QR controller

---
+++ src/main/java/com/tournabay/api/controller/QualificationRoomController.java
@@ -0,0 +1,97 @@
+package com.tournabay.api.controller;
+
+import com.tournabay.api.model.StaffMember;
+import com.tournabay.api.model.Tournament;
+import com.tournabay.api.model.User;
+import com.tournabay.api.model.qualifications.QualificationRoom;
+import com.tournabay.api.payload.UpdateQualificationRoomRequest;
+import com.tournabay.api.security.CurrentUser;
+import com.tournabay.api.security.UserPrincipal;
+import com.tournabay.api.service.QualificationRoomService;
+import com.tournabay.api.service.StaffMemberService;
+import com.tournabay.api.service.TournamentService;
+import com.tournabay.api.service.UserService;
+import com.tournabay.api.payload.CreateQualificationRoomRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/qualification-room")
+public class QualificationRoomController {
+    private final TournamentService tournamentService;
+    private final UserService userService;
+    private final QualificationRoomService qualificationRoomService;
+    private final StaffMemberService staffMemberService;
+
+    @GetMapping("/tournament/{tournamentId}")
+    public ResponseEntity<?> getQualificationRooms(@PathVariable Long tournamentId) {
+        Tournament tournament = tournamentService.getTournamentById(tournamentId);
+        return ResponseEntity.ok(qualificationRoomService.getQualificationRooms(tournament));
+    }
+
+    @PostMapping("/create/{tournamentId}")
+    public ResponseEntity<QualificationRoom> createQualificationRoom(
+            @CurrentUser UserPrincipal userPrincipal,
+            @PathVariable Long tournamentId,
+            @RequestBody CreateQualificationRoomRequest body
+    ) {
+        Tournament tournament = tournamentService.getTournamentById(tournamentId);
+        QualificationRoom qualificationRoom = qualificationRoomService.createQualificationRoom(body.getStartTime(), tournament);
+        return ResponseEntity.ok(qualificationRoom);
+    }
+
+    @PutMapping("/update/{qualificationRoomId}/tournament/{tournamentId}")
+    public ResponseEntity<QualificationRoom> updateQualificationRoom(
+            @CurrentUser UserPrincipal userPrincipal,
+            @PathVariable Long qualificationRoomId,
+            @PathVariable Long tournamentId,
+            @RequestBody UpdateQualificationRoomRequest body
+    ) {
+        Tournament tournament = tournamentService.getTournamentById(tournamentId);
+        QualificationRoom qualificationRoom = qualificationRoomService.getQualificationRoom(qualificationRoomId, tournament);
+        qualificationRoom = qualificationRoomService.updateQualificationRoom(tournament, qualificationRoom, body);
+        return ResponseEntity.ok(qualificationRoom);
+    }
+
+    @DeleteMapping("/remove/{qualificationRoomId}/tournament/{tournamentId}")
+    public ResponseEntity<QualificationRoom> removeQualificationRoom(
+            @CurrentUser UserPrincipal userPrincipal,
+            @PathVariable Long qualificationRoomId,
+            @PathVariable Long tournamentId
+    ) {
+        Tournament tournament = tournamentService.getTournamentById(tournamentId);
+        QualificationRoom qualificationRoom = qualificationRoomService.getQualificationRoom(qualificationRoomId, tournament);
+        qualificationRoom = qualificationRoomService.removeQualificationRoom(qualificationRoom, tournament);
+        return ResponseEntity.ok(qualificationRoom);
+    }
+
+    @PutMapping("/add-staff-member/{qualificationRoomId}/tournament/{tournamentId}")
+    public ResponseEntity<QualificationRoom> addStaffMember(
+            @CurrentUser UserPrincipal userPrincipal,
+            @PathVariable Long qualificationRoomId,
+            @PathVariable Long tournamentId
+    ) {
+        Tournament tournament = tournamentService.getTournamentById(tournamentId);
+        QualificationRoom qualificationRoom = qualificationRoomService.getQualificationRoom(qualificationRoomId, tournament);
+        User user = userService.getUserFromPrincipal(userPrincipal);
+        StaffMember staffMember = staffMemberService.getStaffMemberByUser(user, tournament);
+        qualificationRoom = qualificationRoomService.addStaffMember(qualificationRoom, staffMember);
+        return ResponseEntity.ok(qualificationRoom);
+    }
+
+    @PutMapping("/remove-staff-member/{qualificationRoomId}/tournament/{tournamentId}")
+    public ResponseEntity<QualificationRoom> removeStaffMember(
+            @CurrentUser UserPrincipal userPrincipal,
+            @PathVariable Long qualificationRoomId,
+            @PathVariable Long tournamentId
+    ) {
+        Tournament tournament = tournamentService.getTournamentById(tournamentId);
+        QualificationRoom qualificationRoom = qualificationRoomService.getQualificationRoom(qualificationRoomId, tournament);
+        User user = userService.getUserFromPrincipal(userPrincipal);
+        StaffMember staffMember = staffMemberService.getStaffMemberByUser(user, tournament);
+        qualificationRoom = qualificationRoomService.removeStaffMember(qualificationRoom, staffMember);
+        return ResponseEntity.ok(qualificationRoom);
+    }
+}
