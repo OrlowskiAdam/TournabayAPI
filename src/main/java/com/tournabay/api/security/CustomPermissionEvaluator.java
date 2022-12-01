04d4200f6315979c31cda095592b6bc83a7d5ccd
Adam Orłowski <orlowski.adam.99@gmail.com>
2022-12-02 00:51:57 +0100

    Optimized imports

--- src/main/java/com/tournabay/api/security/CustomPermissionEvaluator.java
+++ src/main/java/com/tournabay/api/security/CustomPermissionEvaluator.java
@@ -9,7 +9,6 @@ import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
 
 import java.io.Serializable;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 @RequiredArgsConstructor
